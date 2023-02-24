4415716153eb5e7eb80d7fea8d221b9749b18ab9
Evolusin <98921577+Evolusin@users.noreply.github.com>
2023-02-24 23:42:43 +0100

    Formated with black

--- snap/get_cards_on_hand.py
+++ snap/get_cards_on_hand.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import mss
-import time
 import os
 
 
@@ -13,20 +12,17 @@ def get_templates():
 
 
 def find_templates_on_screenshot(screenshot, templates):
-    # Wczytanie obrazów
     img = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-
-    # Wykonujemy template matching dla każdego szablonu
     matches = {}
     for x in templates:
         template = cv2.imread(f"templates/{x}", cv2.IMREAD_GRAYSCALE)
         res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
 
-        # Określamy próg, powyżej którego uznajemy, że template pasuje
+        # Setting a threshold
         threshold = 0.5
         loc = np.where(res >= threshold)
 
-        # Dla każdego dopasowania rysujemy prostokąt i dodajemy do wyniku
+        # For each match we draw a rectangle around it on our screenshot
         rects = []
         for pt in zip(*loc[::-1]):
             rects.append((pt[0], pt[1], template.shape[1], template.shape[0]))
@@ -49,37 +45,49 @@ def get_screenshot(monitor):
 
 
 def draw_matches(screen, matches, main_screen=True):
-    # Rysujemy wyniki na obrazie i wyświetlamy go
+    # draw rectangles and text on screenshot
     for template, rects in matches.items():
         for rect in rects:
             pt = rect[0], rect[1]
             if main_screen:
-                pt = (pt[0], pt[1] + 740)  # dodajemy współrzędne górnego lewego rogu okna "screenshot"
-            cv2.rectangle(screen, pt, (pt[0] + rect[2], pt[1] + rect[3]), (0, 0, 255), 2)
+                pt = (
+                    pt[0],
+                    pt[1] + 740,
+                )  # add 740 to y position because we cut image
+            cv2.rectangle(
+                screen, pt, (pt[0] + rect[2], pt[1] + rect[3]), (0, 0, 255), 2
+            )
             text = template.replace(".png", "")
             org = (pt[0], pt[1] + rect[3] + 20)
-            cv2.putText(screen, text, org, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(
+                screen,
+                text,
+                org,
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (0, 0, 255),
+                2,
+                cv2.LINE_AA,
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     monitor = {"top": 38, "left": 623, "width": 555, "height": 1030}
-
     templates = get_templates()
 
     while True:
-        # Pobieramy zrzut ekranu
+        # get screenshot from monitor
         screen = get_screenshot(monitor)
         # create new screenshot from point (x, y) to (x1, y1)
         screenshot = cut_image(screen, 0, 740, 555, 900)
-
-        # Wyszukujemy template'y na zrzucie ekranu
+        # search templates on cutted screenshot
         matches = find_templates_on_screenshot(screenshot, templates)
 
         draw_matches(screen, matches)
         draw_matches(screenshot, matches, main_screen=False)
 
-        cv2.imshow('Screen', screen)
-        cv2.imshow('Screenshot', screenshot)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imshow("Screen", screen)
+        cv2.imshow("Screenshot", screenshot)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
