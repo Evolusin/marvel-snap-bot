6eac1266eb0f9c6485ca2a169e5aef733d35fd52
Evolusin <98921577+Evolusin@users.noreply.github.com>
2023-03-16 23:10:39 +0100

    Added mana pool

--- snap/logic.py
+++ snap/logic.py
@@ -1,6 +1,8 @@
 from get_cards_on_hand import analize_hand
+from state import analize_mana
 from utilts.images import show_images
 
 def game_logic():
     screen, screenshot, matches, count, cards_on_hand = analize_hand()
+    screen, screenshot, matches = analize_mana(screen, screenshot)
     show_images([screen, screenshot], ["Screen", "Screenshot"])
